efa08c58f5b19b0584f30ea5fc9ebbf56c5eb1e8
Peejay Garcia <peejgarcia@gmail.com>
2018-12-06 21:32:46 -0600

    Added Javascript
    Added Javascript!

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Hi! Welcome to Aaron's Construction site!")
+var firstName = prompt("What is your name?")
+var lastName = prompt("What is your last name?")		 
+    {alert("I hope we can help you" +' '+ firstName + ' ' + lastName + '.')}
